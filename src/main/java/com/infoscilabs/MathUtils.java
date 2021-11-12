9c1202cde0ab78c94c6d70ce21cebe5537b3b607
mohanshenoy <mohanshenoy@gmail.com>
2021-11-12 22:20:24 +0530

    Using annotations: DisplayName, Disabled  & methods: assumeTrue , assertAll

--- src/main/java/com/infoscilabs/MathUtils.java
+++ src/main/java/com/infoscilabs/MathUtils.java
@@ -9,6 +9,10 @@ public class MathUtils {
 	public int divide(int a , int b ){
 		return a/b;
 	}
+
+	public int multiply(int a , int b ){
+		return a*b;
+	}
 	
 	public double computeCircleArea(double radius) {
 		return Math.PI * radius * radius;
--- src/test/java/com/infoscilabs/MathUtilsTest.java
+++ src/test/java/com/infoscilabs/MathUtilsTest.java
@@ -1,11 +1,14 @@
 package com.infoscilabs;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
@@ -36,6 +39,7 @@ class MathUtilsTest {
 	}
 	
 	@Test
+	@DisplayName(value = "test the ADD method")
 	void testAdd() {
 		int expected = 10;
 		int actual = mathUtils.add(5,5);
@@ -43,12 +47,28 @@ class MathUtilsTest {
 	}
 
 	@Test
+	@DisplayName(value = "test the DIVIDE method")
 	void testDivide() {
 		assertThrows(ArithmeticException.class, () -> mathUtils.divide(5,0) ,"divide by zero should throw");
 	}
+
+	@Test
+	@DisplayName(value = "test the MULTIPLY method")
+	void testMultiply() {
+		//assertThrows(ArithmeticException.class, () -> mathUtils.divide(5,0) ,"divide by zero should throw");
+		assertAll(
+		() -> assertEquals(4,  mathUtils.multiply(2,2)),
+		() -> assertEquals(0,  mathUtils.multiply(2,0)),
+		() -> assertEquals(-2,  mathUtils.multiply(2,-1))
+		);
+	}
 	
 	@Test
+	@DisplayName(value = "test the COMPUTE CIRCLE AREA method")
+	//@Disabled
 	void testComputeCircleArea() {
+		boolean isServerUp = true;
+		assumeTrue(isServerUp);
 		double expected = 314.1592653589793;
 		double actual = mathUtils.computeCircleArea(10);
 		assertEquals(expected, actual, "should return right area");
